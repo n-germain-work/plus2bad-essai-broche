bbfda0c7a2a2668834da7008d38bbbb02c582265
Nicolas GERMAIN <n.germain.work@gmail.com>
2021-07-04 00:10:05 +0200

    ajout delete row & column

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
   //Permet de faire un map pour créer autant de div 'row' que nécessaire
   const [rowsArray, setRowsArray] = useState([]);
 
-  const handleCreate = () => {
-    if (columns <= 0 || rows <= 0) {
+  const handleCreate = (c, r, existingData) => {
+    if (c <= 0 || r <= 0) {
       alert('Columns AND rows must be superior to 0 ;)');
     } else {
       //Numéro de card
@@ -23,10 +23,10 @@ function App() {
       let temp = [];
       //Etat temporaire de rowsArray
       let tempRows = [];
-      for (let i = 1; i <= rows; i++) {
+      for (let i = 1; i <= r; i++) {
         //Ajoute le numéro de la ligne
         tempRows.push(i);
-        for (let o = 1; o <= columns; o++) {
+        for (let o = 1; o <= c; o++) {
           //Ajoute une card dans la row
           temp.push({
             num: num,
@@ -43,10 +43,55 @@ function App() {
           num++;
         }
       }
+      //Si la fonction reçoit un tableau existant (add/delete rows/columns)
+      if (existingData) {
+        for (let i = 0; i < temp.length; i++) {
+          for (let o = 0; o < existingData.length; o++) {
+            //Si les position x et y des cartes coïncident
+            if (
+              temp[i].x === existingData[o].x &&
+              temp[i].y === existingData[o].y
+            ) {
+              //Remettre toutes les données pertinentes
+              temp[i].active = existingData[o].active;
+              temp[i].item1 = existingData[o].item1;
+              temp[i].item1Active = existingData[o].item1Active;
+              temp[i].item2 = existingData[o].item2;
+              temp[i].item2Active = existingData[o].item2Active;
+            }
+          }
+        }
+      }
       //Mets à jour les states
       setRowsArray(tempRows);
       setWall(temp);
     }
+  };
+
+  const handleAddRow = () => {
+    //Incremente le state
+    setRows(parseInt(rows) + 1);
+    //Relance une création totale, en contournant l'asynchronisme du state
+    handleCreate(columns, parseInt(rows) + 1, wall);
+  };
+
+  const handleAddColumn = () => {
+    setColumns(parseInt(columns) + 1);
+    handleCreate(parseInt(columns) + 1, rows, wall);
+  };
+
+  const handleDeleteRow = () => {
+    let tempRows = parseInt(rows) - 1;
+    if (tempRows === 0) tempRows = 1;
+    setRows(tempRows);
+    handleCreate(columns, tempRows, wall);
+  };
+
+  const handleDeleteColumn = () => {
+    let tempColumns = parseInt(columns) - 1;
+    if (tempColumns === 0) tempColumns = 1;
+    setColumns(tempColumns);
+    handleCreate(tempColumns, rows, wall);
   };
 
   return (
@@ -70,9 +115,47 @@ function App() {
             onChange={(e) => setRows(e.target.value)}
           ></input>
         </label>
-        <button type='button' className='creationButton' onClick={handleCreate}>
+        <button
+          type='button'
+          className='creationButton'
+          onClick={() => handleCreate(columns, rows)}
+        >
           Create
         </button>
+
+        <div className='addButtons'>
+          <button
+            type='button'
+            className='creationButton'
+            onClick={handleAddRow}
+          >
+            Add row
+          </button>
+          <button
+            type='button'
+            className='creationButton'
+            onClick={handleAddColumn}
+          >
+            Add column
+          </button>
+        </div>
+
+        <div className='deleteButtons'>
+          <button
+            type='button'
+            className='creationButton'
+            onClick={handleDeleteRow}
+          >
+            Delete last row
+          </button>
+          <button
+            type='button'
+            className='creationButton'
+            onClick={handleDeleteColumn}
+          >
+            Delete last column
+          </button>
+        </div>
       </div>
 
       {rowsArray.map((row) => (

db31474a585cd537622f7dc8a419331408536ef1
Nicolas GERMAIN <n.germain.work@gmail.com>
2021-07-03 15:20:16 +0200

    version paramétrable

--- src/App.js
+++ src/App.js
@@ -2,159 +2,88 @@ import { useState } from 'react';
 import './App.css';
 import Card from './components/Card';
 
-const arrayWall = [
-  {
-    num: 1,
-    type: 'raquette',
-    x: 1,
-    y: 1,
-    active: true,
-    item1: 'ref1',
-    item1Active: true,
-    item2: 'ref2',
-    item2Active: true,
-  },
-  {
-    num: 2,
-    type: 'raquette',
-    x: 2,
-    y: 1,
-    active: true,
-    item1: 'ref3',
-    item1Active: true,
-    item2: 'ref4',
-    item2Active: true,
-  },
-  {
-    num: 3,
-    type: 'raquette',
-    x: 3,
-    y: 1,
-    active: true,
-    item1: 'ref5',
-    item1Active: true,
-    item2: 'ref6',
-    item2Active: true,
-  },
-  {
-    num: 4,
-    type: 'raquette',
-    x: 4,
-    y: 1,
-    active: true,
-    item1: 'ref7',
-    item1Active: true,
-    item2: 'ref8',
-    item2Active: true,
-  },
-  {
-    num: 5,
-    type: 'raquette',
-    x: 5,
-    y: 1,
-    active: true,
-    item1: 'ref9',
-    item1Active: true,
-    item2: 'ref10',
-    item2Active: true,
-  },
-  {
-    num: 6,
-    type: 'raquette',
-    x: 6,
-    y: 1,
-    active: true,
-    item1: 'ref11',
-    item1Active: true,
-    item2: 'ref12',
-    item2Active: true,
-  },
-  {
-    num: 7,
-    type: 'raquette',
-    x: 1,
-    y: 2,
-    active: true,
-    item1: 'ref13',
-    item1Active: true,
-    item2: 'ref14',
-    item2Active: true,
-  },
-  {
-    num: 8,
-    type: 'raquette',
-    x: 2,
-    y: 2,
-    active: true,
-    item1: 'ref15',
-    item1Active: true,
-    item2: 'ref16',
-    item2Active: true,
-  },
-  {
-    num: 9,
-    type: 'raquette',
-    x: 3,
-    y: 2,
-    active: true,
-    item1: 'ref17',
-    item1Active: true,
-    item2: 'ref18',
-    item2Active: true,
-  },
-  {
-    num: 10,
-    type: 'raquette',
-    x: 4,
-    y: 2,
-    active: true,
-    item1: 'ref19',
-    item1Active: true,
-    item2: 'ref20',
-    item2Active: true,
-  },
-  {
-    num: 11,
-    type: 'raquette',
-    x: 5,
-    y: 2,
-    active: true,
-    item1: 'ref21',
-    item1Active: true,
-    item2: 'ref22',
-    item2Active: true,
-  },
-  {
-    num: 12,
-    type: 'raquette',
-    x: 6,
-    y: 2,
-    active: true,
-    item1: 'ref23',
-    item1Active: true,
-    item2: 'ref24',
-    item2Active: true,
-  },
-];
-
 function App() {
-  const [wall, setWall] = useState(arrayWall);
+  //Stocke le nombre de colonnes de cards
+  const [columns, setColumns] = useState(0);
+  //Stocke le nombre de lignes de cards
+  const [rows, setRows] = useState(0);
+  //Stocke l'état global du mur
+  const [wall, setWall] = useState([]);
+  //Stocke un tableau contenant le numéro de chaque ligne
+  //Permet de faire un map pour créer autant de div 'row' que nécessaire
+  const [rowsArray, setRowsArray] = useState([]);
+
+  const handleCreate = () => {
+    if (columns <= 0 || rows <= 0) {
+      alert('Columns AND rows must be superior to 0 ;)');
+    } else {
+      //Numéro de card
+      let num = 1;
+      //Etat temporaire de wall
+      let temp = [];
+      //Etat temporaire de rowsArray
+      let tempRows = [];
+      for (let i = 1; i <= rows; i++) {
+        //Ajoute le numéro de la ligne
+        tempRows.push(i);
+        for (let o = 1; o <= columns; o++) {
+          //Ajoute une card dans la row
+          temp.push({
+            num: num,
+            type: 'raquette',
+            x: i,
+            y: o,
+            active: true,
+            item1: null,
+            item1Active: false,
+            item2: null,
+            item2Active: false,
+          });
+          //Incremente le numéro de card
+          num++;
+        }
+      }
+      //Mets à jour les states
+      setRowsArray(tempRows);
+      setWall(temp);
+    }
+  };
+
   return (
     <div className='App'>
-      <div className='row'>
-        {wall
-          .filter((space) => space.y === 1)
-          .map((space) => (
-            <Card space={space} wall={wall} setWall={setWall} />
-          ))}
+      <div className='creationSpace'>
+        <label htmlFor='columns'>
+          _Columns_:
+          <input
+            name='columns'
+            value={columns}
+            className='inputCreation'
+            onChange={(e) => setColumns(e.target.value)}
+          ></input>
+        </label>
+        <label htmlFor='rows'>
+          _Rows_____:
+          <input
+            name='rows'
+            value={rows}
+            className='inputCreation'
+            onChange={(e) => setRows(e.target.value)}
+          ></input>
+        </label>
+        <button type='button' className='creationButton' onClick={handleCreate}>
+          Create
+        </button>
       </div>
-      <div className='row'>
-        {wall
-          .filter((space) => space.y === 2)
-          .map((space) => (
-            <Card space={space} wall={wall} setWall={setWall} />
-          ))}
-      </div>
+
+      {rowsArray.map((row) => (
+        <div className='row'>
+          {wall
+            .filter((space) => space.x === row)
+            .map((space) => (
+              <Card space={space} wall={wall} setWall={setWall} />
+            ))}
+        </div>
+      ))}
     </div>
   );
 }
